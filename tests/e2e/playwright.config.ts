3fca9f3be52956332fe4d0584eabd39787f6f619
Устименко Роман Олексійович <ustimenko_ro@corp.imp.lg.ua>
2025-06-18 17:04:35 +0300

    updated test.yml E2E section

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './',
   use: {
-    baseURL: process.env.APP_BASE_URL || 'http://api:8080',
+    baseURL: process.env.APP_BASE_URL || 'http://localhost:8080/',
   },
 })
