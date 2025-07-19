b41d5a89cd54913cec04b8b075250a9bc79e1df0
unknown <raustime@gmail.com>
2025-07-19 21:49:23 +0300

    added tests

---
+++ tests/integration/weather_test.go
@@ -0,0 +1,16 @@
+package integration
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWeatherService_HTTP(t *testing.T) {
+	client := http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Get("http://weather_service:8080/api/weather?city=Kyiv")
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+}
