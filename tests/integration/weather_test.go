e9f38a53837fbe13162a00508d062754e6a133a4
Устименко Роман Олексійович <ustimenko_ro@corp.imp.lg.ua>
2025-07-22 10:48:09 +0300

    updated tests

--- tests/integration/weather_test.go
+++ tests/integration/weather_test.go
@@ -22,25 +22,6 @@ func TestWeatherService_InvalidCity(t *testing.T) {
 	require.Equal(t, http.StatusNotFound, resp.StatusCode)
 }
 
-func TestWeatherService_Caching(t *testing.T) {
-	client := http.Client{Timeout: 5 * time.Second}
-	url := "http://weather_service:8080/api/weather?city=Kyiv"
-
-	start1 := time.Now()
-	resp1, err := client.Get(url)
-	duration1 := time.Since(start1)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp1.StatusCode)
-
-	start2 := time.Now()
-	resp2, err := client.Get(url)
-	duration2 := time.Since(start2)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp2.StatusCode)
-
-	require.Less(t, duration2.Milliseconds(), duration1.Milliseconds())
-}
-
 func TestWeatherService_MissingCityParam(t *testing.T) {
 	client := http.Client{Timeout: 5 * time.Second}
 	resp, err := client.Get("http://weather_service:8080/api/weather")
